7a283152bcbbee576d7a8f189afdc3ed27825a72
shruti03072004 <shruti.dakhore@gmail.com>
2025-12-20 18:29:22 +0530

    fix https api url

--- Angular/src/environments/environment.ts
+++ Angular/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://ridebee-env.eba-ts9mn8pe.eu-north-1.elasticbeanstalk.com'
+  apiUrl: 'https://ridebee-env.eba-ts9mn8pe.eu-north-1.elasticbeanstalk.com'
 };
